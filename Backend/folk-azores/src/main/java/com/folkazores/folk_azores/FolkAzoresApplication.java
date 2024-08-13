dbbeb29b8186a148f4c5b151bd9321e165ae68fc
Ricardo Simoes <ricardosimoess2001@gmail.com>
2024-08-13 17:45:38 +0000

    first commit

---
+++ Backend/folk-azores/src/main/java/com/folkazores/folk_azores/FolkAzoresApplication.java
@@ -0,0 +1,13 @@
+package com.folkazores.folk_azores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkAzoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolkAzoresApplication.class, args);
+	}
+
+}
---
+++ Backend/folk-azores/src/test/java/com/folkazores/folk_azores/FolkAzoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folkazores.folk_azores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkAzoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
