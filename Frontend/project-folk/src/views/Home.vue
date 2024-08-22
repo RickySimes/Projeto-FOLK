da78434825256bf597197809c63846a4805146be
Ricardo Simoes <ricardosimoess2001@gmail.com>
2024-08-22 17:03:09 +0000

    added login page

--- Frontend/project-folk/src/views/Home.vue
+++ Frontend/project-folk/src/views/Home.vue
@@ -2,34 +2,43 @@
   <div class="flex flex-col min-h-screen">
     <!-- Header -->
     <header class="bg-gradient-to-r from-blue-600 to-purple-900 text-white py-4 shadow-lg">
-      <nav class="container mx-auto flex justify-center items-center space-x-8">
-        <ul class="flex space-x-8 md:space-x-24 font-folk text-sm md:text-base">
+      <nav class="container mx-auto flex justify-between items-center">
+        <div class="w-1/3"></div>
+
+        <!-- Navigation Links -->
+        <ul class="flex space-x-8 md:space-x-24 font-folk text-sm md:text-base justify-center w-1/3">
           <li><router-link to="/" class="hover:text-gray-200">Home</router-link></li>
-          <li><a target="_blank" href="https://issuu.com/cofit/docs/folk_azores_2024_cartaz_a4_web" class="hover:text-gray-200">Programa</a></li>
+          <li>
+            <a target="_blank" href="https://issuu.com/cofit/docs/folk_azores_2024_cartaz_a4_web"
+              class="hover:text-gray-200">Programa</a>
+          </li>
           <li><router-link to="/Volunteer" class="hover:text-gray-200">Voluntariado</router-link></li>
           <li><a href="#" class="hover:text-gray-200">História</a></li>
         </ul>
+
+        <div class="flex items-center justify-end w-1/3">
+          <router-link to="/login" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Login
+          </router-link>
+        </div>
       </nav>
     </header>
 
     <main class="flex-grow relative">
       <div class="absolute inset-0 overflow-hidden z-0">
         <div class="carousel-container overflow-hidden h-full w-full relative">
-          <div ref="carousel" class="flex transition-transform duration-500 ease-in-out h-full" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
-            <img
-              v-for="(image, index) in images"
-              :key="index"
-              :src="image"
-              class="w-full object-cover h-full flex-shrink-0"
-              :alt="'Background Image ' + (index + 1)"
-            />
+          <div ref="carousel" class="flex transition-transform duration-500 ease-in-out h-full"
+            :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+            <img v-for="(image, index) in images" :key="index" :src="image"
+              class="w-full object-cover h-full flex-shrink-0" :alt="'Background Image ' + (index + 1)" />
           </div>
         </div>
       </div>
 
       <!-- Main Content Layered on Top of Carousel -->
       <div class="absolute inset-0 flex items-center justify-center z-10">
-        <div class="flex flex-col items-center justify-center text-white font-folk text-center bg-black bg-opacity-80 p-4 w-full h-full">
+        <div
+          class="flex flex-col items-center justify-center text-white font-folk text-center bg-black bg-opacity-80 p-4 w-full h-full">
           <h1 class="text-5xl md:text-7xl lg:text-9xl font-bold mb-2 select-none">FOLK</h1>
           <h1 class="text-5xl md:text-7xl lg:text-9xl font-bold mt-2 select-none">AZORES</h1>
         </div>
@@ -40,8 +49,10 @@
     <footer class="bg-gradient-to-r from-blue-600 to-purple-900 text-white py-4 shadow-lg">
       <nav class="container mx-auto flex justify-center items-center space-x-8">
         <ul class="flex space-x-8 md:space-x-24 font-folk text-sm md:text-base">
-          <li><a target="_blank" href="https://www.facebook.com/folkazores" class="hover:text-gray-200">Facebook</a></li>
-          <li><a target="_blank" href="https://www.instagram.com/folk.azores/" class="hover:text-gray-200">Instagram</a></li>
+          <li><a target="_blank" href="https://www.facebook.com/folkazores" class="hover:text-gray-200">Facebook</a>
+          </li>
+          <li><a target="_blank" href="https://www.instagram.com/folk.azores/" class="hover:text-gray-200">Instagram</a>
+          </li>
         </ul>
       </nav>
     </footer>
@@ -70,11 +81,10 @@ export default {
       }, 7001);
     },
     next() {
-      // Move to the next image
       if (this.currentIndex < this.images.length - 1) {
         this.currentIndex++;
       } else {
-        this.currentIndex = 0; // Loop back to the first image
+        this.currentIndex = 0;
       }
     },
   }
---
+++ Frontend/project-folk/src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+
+
+    <header
+        class="bg-gradient-to-r from-blue-600 to-purple-900 text-white py-4 shadow-lg fixed top-0 left-0 w-full z-50">
+        <nav class="container mx-auto flex justify-center items-center space-x-8">
+            <ul class="flex space-x-8 md:space-x-24 font-folk text-sm md:text-base">
+                <li><router-link to="/" class="hover:text-gray-200">Home</router-link></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center font-folk">
+        <div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+            <h1 class="text-center text-2xl font-bold">Folk Azores</h1>
+            <form @submit.prevent="handleLogin">
+                <div class="mt-6">
+                    <label for="email" class="block text-sm font-medium text-gray-700">Utilizador</label>
+                    <input v-model="email" type="email" id="email" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                </div>
+                <div class="mt-6">
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <input v-model="password" type="password" id="password" required
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                </div>
+                <div class="flex items-center justify-between mt-4">
+                </div>
+                <div>
+                    <button type="submit"
+                        class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        handleLogin() {
+            const response = fetch('http://localhost:8080/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: this.username, password: this.password }),
+            });
+
+            if (response.ok) {
+                const data = response.json();
+                localStorage.setItem('token', data.token);
+            } else {
+                console.error('Login failed');
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+@media (min-width: 768px) {
+    .max-w-md {
+        width: 100%;
+        max-width: 28rem;
+    }
+}
+</style>
